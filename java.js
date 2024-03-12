6601565bd93ed08438ad12ec1693d9ff9bb83845
AlexVieira1 <157771387+AlexVieira1@users.noreply.github.com>
2024-03-12 18:53:14 -0400

    editado

--- java.js
+++ java.js
@@ -1,31 +1,31 @@
-var valorTotal = [0,0]
-var valorProduto = [39.89,45.89]
-var qtd = [0,0]
- 
-function adicionar(item){
+var valorTotal = [0, 0]
+var valorProduto = [39.89, 45.89]
+var qtd = [0, 0]
+
+function adicionar(item) {
     var quantidade = document.getElementById("quantidade" + item)
-    var total = document.getElementById("total" +item)
-    qtd [item] += 1
+    var total = document.getElementById("total" + item)
+    qtd[item] += 1
     valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item]
     quantidade.innerHTML = qtd[item]
     total.innerHTML = valorTotal[item].toFixed(2)
     valorCompra()
 }
-function removerItem(item){
-    if(qtd[item]>0){
-        qtd[item]-=1 .toFixed(2)
-        var quantidade=document.getElementById("quantidade" +item)
-        var total = document.getElementById("total" +item)
+function removerItem(item) {
+    if (qtd[item] > 0) {
+        qtd[item] -= 1 .toFixed(2)
+        var quantidade = document.getElementById("quantidade" + item)
+        var total = document.getElementById("total" + item)
         quantidade.innerHTML = qtd[item]
-        valorTotal[item]= Number.parseFloat(valorProduto[item])*qtd[item]
+        valorTotal[item] = Number.parseFloat(valorProduto[item]) * qtd[item]
         total.innerHTML = valorTotal[item].toFixed(2)
         valorCompra()
     }
 }
-function valorCompra(){
-    var valorTotalCompra=document.getElementById("valorTotalCompra")
+function valorCompra() {
+    var valorTotalCompra = document.getElementById("valorTotalCompra")
     var valor = 0
-    for(var i = 0; i < valorTotal.length; i++){
+    for (var i = 0; i < valorTotal.length; i++) {
         valor += valorTotal[i]
     }
     valorTotalCompra.innerHTML = valor.toFixed(2)
